342c53c54dfe931f377ed076cf2acc18dabfca81
natx <natx@NATX-PC>
2014-10-17 00:40:46 +0200

    Versió 0.2 del projecte prjava01

--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -19,6 +19,7 @@ public class Prjava01 {
     public static void main(String[] args) throws IOException{
         File f = new File("fitxer.html");
         try (BufferedWriter bw = new BufferedWriter (new FileWriter(f))){
+            bw.write("   <h1>DAW2 m08uf4pr2</h1>");bw.newLine(); 
             bw.write("<html>");bw.newLine();
             bw.write("  <head>");bw.newLine();
             bw.write("    <title>");bw.newLine();
